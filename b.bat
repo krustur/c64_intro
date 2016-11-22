35380b5acd382c8d41b0aa06f318bd107c9ccff6
Krister Jansson <krustur@gmail.com>
2016-11-22 23:32:42 +0100

    Changed build process

--- b.bat
+++ b.bat
@@ -1,4 +1,6 @@
 @echo off
 REM "E:\Emulation\WinVICE-2.4-x64\petcat.exe" -w2 basicheader.bas >basicheader.obj
-"F:\Google Drive\Projects\_others\src\ACME\Debug\ACME.exe" -f cbm --cpu 6502 --vicelabels output\gubbhack.vl -o output\gubbhack.prg src\gubbhack.asm 
+pushd src
+"..\bin\ACME\ACME.exe" -f cbm --cpu 6502 --vicelabels ..\output\gubbhack.vl -o ..\output\gubbhack.prg gubbhack.asm 
+popd
 "E:\Emulation\WinVICE-2.4-x64\c1541.exe" -format GUBBHACK,1 d64 output\gubbhack.d64 -attach output\gubbhack.d64 -write output\gubbhack.prg gubbhack
