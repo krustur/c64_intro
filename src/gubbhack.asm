fffd5d052782a38943a542bd17c401080413ee4b
Krister Jansson <krustur@gmail.com>
2016-11-27 23:26:36 +0100

    Made a BasicLoader macro

--- src/gubbhack.asm
+++ src/gubbhack.asm
@@ -84,23 +84,24 @@ COLOR_LIGHT_GREY	= $F
 	sta VICII_BACKGROUND_COLOR_0
 }
 
+!macro BasicLoader startAddress {
 ;// BASIC header with a SYS call 
-* = $0801
 basicLoader
 	!word basicLoaderNextLine
 	!word 10						;// BASIC line number 
 	!byte $9e,$20					;// "SYS <address>"	
-	!byte <(((start/10000)%10)+$30)
-	!byte <(((start/1000)%10)+$30)
-	!byte <(((start/100)%10)+$30)
-	!byte <(((start/10)%10)+$30)
-	!byte <(((start/1)%10)+$30)
+	!byte <(((startAddress/10000)%10)+$30)
+	!byte <(((startAddress/1000)%10)+$30)
+	!byte <(((startAddress/100)%10)+$30)
+	!byte <(((startAddress/10)%10)+$30)
+	!byte <(((startAddress/1)%10)+$30)
 	!byte $00 						;// BASIC eol
 basicLoaderNextLine
 	!word 0,0						;// BASIC end marker 
+}
 
-
-
+* = $0801
+	+BasicLoader start
 
 ;//* = $c000	;// start address for 6502 code
 start	
