3e6d487fd6662109f92913fcae9a87088b8166db
Krister Jansson <krustur@gmail.com>
2016-12-31 02:30:00 +0100

    Added some comments to build bat file

--- b.bat
+++ b.bat
@@ -1,6 +1,8 @@
 @echo off
-REM "E:\Emulation\WinVICE-2.4-x64\petcat.exe" -w2 basicheader.bas >basicheader.obj
+
 pushd src
+REM Assemble to gubbhack.prg
 "..\bin\ACME\ACME.exe" -f cbm --cpu 6502 --vicelabels ..\output\gubbhack.vl -o ..\output\gubbhack.prg gubbhack.asm 
 popd
+REM Format d64-file with gubbhack.prg
 "E:\Emulation\WinVICE-2.4-x64\c1541.exe" -format GUBBHACK,1 d64 output\gubbhack.d64 -attach output\gubbhack.d64 -write output\gubbhack.prg gubbhack
